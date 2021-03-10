b39526aa7c16c171e8692cb177d4bfd362589bd6
KhayalT <teyyubovxeyal>
2021-03-10 17:15:49 +0400

    Add project files.

---
+++ vugar8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace vugar8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 1;
+            Console.WriteLine("insert your number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("insert your second number");
+            int top = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < top; i++)
+            {
+                total = total * number;
+
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
